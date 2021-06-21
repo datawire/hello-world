f6e7f8605598804d74faadf0e771b2595adf17f8
Steve Melo <steve.melo@rewardops.com>
2021-06-21 12:39:43 -0400

    added k8s deploymnet, service and secrets yaml files.

--- server.py
+++ server.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
 
 PORT = 8000
 MESSAGE = "Hello, world!\n"
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def root():
     result = MESSAGE.encode("utf-8")
     return result
 
+@app.route("/env/")
+def hello():
+    env = []
+    for item, value in os.environ.items():
+      if item.startswith("APP_CONFIG"):
+        env.append('{}: {}'.format(item, value)) 
+
+    return jsonify(env)
+     
+@app.route("/config/")
+def config():
+    try:
+      with open("k8s-workshop-configmap/app.config", "r") as f:
+        result = f.read()
+    except:
+      result = "File k8s-workshop-configmap/app.config not found."
+
+      return result
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=PORT)
